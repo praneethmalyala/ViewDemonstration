cd109ed886adb90c5b2a0edea63399c46eee0e08
praneethmalyala <praneeth.malyala117>
2020-02-24 09:30:58 -0500

    Initial commit

---
+++ app/src/main/java/com/malyalapraneeth/viewdemonstration/MainActivity.java
@@ -0,0 +1,14 @@
+package com.malyalapraneeth.viewdemonstration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
